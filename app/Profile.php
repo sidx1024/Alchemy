52e99b515230bc349bbb0f7e44ceed8d535f4195
sidx1024 <vistax86live@gmail.com>
2018-01-09 21:50:46 +0530

    Added profile archiving and restoring functions. Added DBG.

--- app/Profile.php
+++ app/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
@@ -22,48 +23,81 @@ class Profile extends Model
 
     public function CourseOffered()
     {
-        return $this->hasMany('App\CourseOffered');
+        if($this->is_archived === 0) {
+            return $this->hasMany('App\CourseOffered');
+        }
+        return $this->hasMany('App\HistoryCourseOffered');
     }
 
     public function CourseOfferedFaculty()
     {
-        return $this->hasMany('App\CourseOfferedFaculty');
+        if($this->is_archived === 0) {
+            return $this->hasMany('App\CourseOfferedFaculty');
+        }
+        return $this->hasMany('App\HistoryCourseOfferedFaculty');
     }
 
     public function TimeTable()
     {
-        return $this->hasMany('App\TimeTable');
+        if($this->is_archived === 0) {
+            return $this->hasMany('App\TimeTable');
+        }
+        return $this->hasMany('App\HistoryTimeTable');
     }
 
     public function archive()
     {
-        if ($this->is_archived === 0) {
-            $this->is_archived = 1;
-            $this->save();
-            return "Profile archived.";
-        } else {
+        if ($this->is_archived === 1) { 
             return "This profile is already archived.";
         }
+
+        DB::beginTransaction();
+
+        $course_offered_array = $this->CourseOffered->toArray();
+        $course_offered_faculty_array = $this->CourseOfferedFaculty->toArray();
+        $time_table_array = $this->TimeTable->toArray();        
+
+        HistoryCourseOffered::insert($course_offered_array);
+        HistoryCourseOfferedFaculty::insert($course_offered_faculty_array);
+        HistoryTimeTable::insert($time_table_array);
+
+        CourseOfferedFaculty::where('profile_id', '=', $this->id)->delete();
+        TimeTable::where('profile_id', '=', $this->id)->delete();
+        CourseOffered::where('profile_id', '=', $this->id)->delete();    
+        
+        $this->is_archived = 1;
+        $this->save();
+
+        DB::commit();
+
+        return "Profile archived.";        
     }
 
     public function restore()
     {
-        if ($this->is_archived === 1) {
-            $this->is_archived = 0;
-            $this->save();
-            return "Profile restored.";
-        } else {
+        if ($this->is_archived === 0) { 
             return "This profile is already restored.";
         }
-    }
 
-    private function archiveData () {
-        $records = 
-        
-        $instances = $this->related->newCollection();
+        DB::beginTransaction();
 
-        foreach ($records as $record) {
-            $instances->push($this->create($record));
-        }
+        $course_offered_array = $this->CourseOffered->toArray();
+        $course_offered_faculty_array = $this->CourseOfferedFaculty->toArray();
+        $time_table_array = $this->TimeTable->toArray();        
+
+        CourseOffered::insert($course_offered_array);
+        CourseOfferedFaculty::insert($course_offered_faculty_array);
+        TimeTable::insert($time_table_array);
+
+        HistoryCourseOfferedFaculty::where('profile_id', '=', $this->id)->delete();
+        HistoryTimeTable::where('profile_id', '=', $this->id)->delete();
+        HistoryCourseOffered::where('profile_id', '=', $this->id)->delete();     
+            
+        $this->is_archived = 0;
+        $this->save();
+
+        DB::commit();
+
+        return "Profile restored.";        
     }
 }
