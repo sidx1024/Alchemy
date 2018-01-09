71d386981be0194b30a650e332864cf550f5a652
sidx1024 <vistax86live@gmail.com>
2018-01-10 00:41:27 +0530

    Added profile duplication function

--- app/Profile.php
+++ app/Profile.php
@@ -100,4 +100,70 @@ class Profile extends Model
 
         return "Profile restored.";        
     }
+
+    public function duplicate($newName) {
+        if($this->is_archived === 0) {
+            return "Only archived profiles can be duplicated.";
+        }
+        if($newName == NULL) {
+            return "Please enter new profile name.";
+        }
+
+        DB::beginTransaction();
+
+        $newProfile = $this->replicate();
+        $newProfile->name = $newName;
+        $newProfile->year = date("Y");
+        $newProfile->is_archived = 0;
+        $newProfile->save();
+        $newProfileId = $newProfile->id;
+
+        $course_offered_array = $this->CourseOffered->toArray();
+        $course_offered_faculty_array = $this->CourseOfferedFaculty->toArray();
+        $time_table_array = $this->TimeTable->toArray();
+
+        $course_offered_max_id = CourseOffered::max('id');
+        $history_course_offered_max_id = HistoryCourseOffered::max('id');
+
+        $course_offered_global_max_id = max($course_offered_max_id, $history_course_offered_max_id, 0);
+        $course_offered_profile_min_id = max($this->CourseOffered->min('id'), 0);
+
+        $insert_id_offset = ($course_offered_global_max_id - $course_offered_profile_min_id) + 1;
+
+        foreach($course_offered_array as &$elem) {
+            $elem['id'] += $insert_id_offset;
+            $elem['profile_id'] = $newProfileId;
+        }
+
+        foreach($course_offered_faculty_array as &$elem) {
+            $elem['course_offered_id'] += $insert_id_offset;
+            $elem['profile_id'] = $newProfileId;
+        }
+
+        foreach($time_table_array as &$elem) {
+            $elem['course_offered_id'] += $insert_id_offset;
+            $elem['profile_id'] = $newProfileId;
+            unset($elem['id']);            
+        }
+
+        CourseOffered::insert($course_offered_array);
+        CourseOfferedFaculty::insert($course_offered_faculty_array);
+        TimeTable::insert($time_table_array);
+
+        DB::commit();
+
+        return $newProfile;
+    }
+
+    public function wipe() {
+        $deletes = 0;
+        $deletes += CourseOfferedFaculty::where('profile_id', '=', $this->id)->delete();
+        $deletes += TimeTable::where('profile_id', '=', $this->id)->delete();
+        $deletes += CourseOffered::where('profile_id', '=', $this->id)->delete();
+        $deletes += HistoryCourseOfferedFaculty::where('profile_id', '=', $this->id)->delete();
+        $deletes += HistoryTimeTable::where('profile_id', '=', $this->id)->delete();
+        $deletes += HistoryCourseOffered::where('profile_id', '=', $this->id)->delete();        
+        $this->delete();
+        return $deletes;
+    }
 }
