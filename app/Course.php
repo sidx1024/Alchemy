535b4f63df39740f54fbd6cb5d4ac13817bf8892
sidx1024 <vistax86live@gmail.com>
2018-02-12 03:54:36 +0530

    Optimized search function

--- app/Course.php
+++ app/Course.php
@@ -35,8 +35,6 @@ class Course extends Model
         return $this->hasMany('App\CourseOffered');
     }
 
-    // TODO: Write a better search function.
-    // TODO: Add comments.
     /**
      * @param null $department_id
      * @param null $level
@@ -45,38 +43,23 @@ class Course extends Model
      */
     public static function Search($department_id = null, $level = null, $text = null)
     {
-        $query = null;
+        $query = Course::take(self::SEARCH_RESULT_LIMIT);
         if (!is_null($department_id)) {
-            $query = Course::where('department_id', $department_id);
+            $query = $query->where('department_id', $department_id);
         }
         if (!is_null($level)) {
             // BVM-specific filter:
             // The 3rd letter in course code is level,
             // So we use __X%, where 'X 'is the level.
             $level = '__' . $level . '%';
-            if (is_null($query)) {
-                $query = Course::where('code', 'like', $level);
-            } else {
-                $query->where('code', 'like', $level);
-            }
+            $query->where('code', 'like', $level);
         }
         if (!is_null($text) && strlen(trim($text)) > 0) {
             $text = '%' . $text . '%';
-            if (is_null($query)) {
-                $query = Course::where(function ($query) use ($text) {
-                    $query->where('code', 'like', $text)
-                        ->orWhere('alias', 'like', $text)
-                        ->orWhere('name', 'like', $text);
-                });
-            } else {
-                $query->where('code', 'like', $text)
-                    ->orWhere('alias', 'like', $text)
-                    ->orWhere('name', 'like', $text);
-            }
+            $query->where('code', 'like', $text)
+                ->orWhere('alias', 'like', $text)
+                ->orWhere('name', 'like', $text);
         }
-        if (is_null($query)) {
-            return Course::take(self::SEARCH_RESULT_LIMIT);
-        }
-        return $query->take(self::SEARCH_RESULT_LIMIT);
+        return $query;
     }
 }
